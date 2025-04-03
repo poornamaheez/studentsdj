9f7771e0d56faae85e44bc572a15736737d1a412
Poorna Maheez Uppala <poornamaheez2000@gmail.com>
2025-04-03 23:02:30 +0530

    Created the project

---
+++ studentsdj/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for studentsdj project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studentsdj.settings')
+
+application = get_asgi_application()
