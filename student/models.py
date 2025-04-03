5270b09a63d905b46b0293fcd69f46ac8036e839
Poorna Maheez Uppala <poornamaheez2000@gmail.com>
2025-04-03 23:13:47 +0530

    added the model to the students app

--- student/models.py
+++ student/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    student_key = models.AutoField(primary_key=True)
+    roll_no = models.CharField(max_length=45, unique=True)
+    name = models.CharField(max_length=45)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    gender = models.BooleanField(null=True, blank=True)  # True for Male, False for Female
+
+    def __str__(self):
+        return self.name
