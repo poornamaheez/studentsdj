96b7c51d426af0a4fb8adc43e0daec6868cfd508
Poorna Maheez Uppala <poornamaheez2000@gmail.com>
2025-04-04 00:40:47 +0530

    added swagger UI

--- studentsdj/settings.py
+++ studentsdj/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'student'
+    'student',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- studentsdj/urls.py
+++ studentsdj/urls.py
@@ -17,7 +17,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Define the schema view
+schema_view = get_schema_view(
+    openapi.Info(
+        title="My API",
+        default_version='v1',
+        description="API for CRUD operations",
+        contact=openapi.Contact(email="poornah6@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('student.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
